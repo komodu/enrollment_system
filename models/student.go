c3d08e091b83326c0cbb422d46a6c4229e157974
Wil Lorenz Dagli <dagliwill@gmail.com>
2024-01-25 18:00:22 +0800

    problem in acc num generator

--- models/student.go
+++ models/student.go
@@ -1,8 +1,6 @@
 package models
 
 import (
-	"fmt"
-	"math/rand"
 	"strconv"
 	"time"
 
@@ -14,18 +12,18 @@ type Student struct {
 	AccountNumber string `uadmin:"read_only"`
 	// AccountNumberID uint
 
-	FullName      string `uadmin:"read_only"`
-	FirstName     string `uadmin:"list_exclude;required"`
-	MiddleInitial string `uadmin:"list_exclude;required"`
-	LastName      string `uadmin:"list_exclude;required"`
-	Age           int    `uadmin:"pattern:^[0-9]*$;required"`
-	Gender        Gender `uadmin:"required"`
-	Address       string `uadmin:"list_exclude;required"`
-	Email         string `uadmin:"email;required"`
-	PSA           string `uadmin:"file;list_exclude"`
-	GoodMoral     string `uadmin:"file;list_exclude"`
-	Form137       string `uadmin:"file;list_exclude"`
-	Guardian      string
+	FullName  string `uadmin:"read_only"`
+	FirstName string `uadmin:"list_exclude"`
+	//MiddleInitial string `uadmin:"list_exclude"`
+	LastName  string `uadmin:"list_exclude"`
+	Age       int    `uadmin:"pattern:^[0-9]*$"`
+	Gender    Gender `uadmin:"required"`
+	Address   string `uadmin:"list_exclude"`
+	Email     string `uadmin:"email"`
+	PSA       string `uadmin:"file;list_exclude"`
+	GoodMoral string `uadmin:"file;list_exclude"`
+	Form137   string `uadmin:"file;list_exclude"`
+	Guardian  string
 	// //AvailableCollegeCourses
 
 	Schools   Schools `uadmin:"help:Where will you enroll?"`
@@ -49,6 +47,32 @@ func (s *Student) String() string {
 	return s.FullName
 }
 
+func (t *Student) Validate() (errMsg map[string]string) {
+	// Initialize the error messages
+	errMsg = map[string]string{}
+	ind := "2400"
+	x := 1000
+	y := uadmin.Count(&t, "account_number <> ?", t.AccountNumber)
+	// Get any records from the database that macthes the name of
+	// this record and make sure the record is not the record we are
+	// editing right now
+	if t.Enrolled {
+
+		if uadmin.Count(&t, "account_number <> ?", t.AccountNumber) != 0 {
+			errMsg["account_number"] = "The Student is already in the system"
+			t.AccountNumber = ind + strconv.Itoa(x) + strconv.Itoa(y)
+			uadmin.Trail(uadmin.DEBUG, t.AccountNumber)
+			// uadmin.ALERT("")
+		} else {
+			//t.AccountNumber = strconv.Itoa(rand.Intn(10))
+			t.AccountNumber = ind + strconv.Itoa(x)
+			uadmin.Trail(uadmin.DEBUG, "The Student is not in the system.")
+		}
+		uadmin.Save(t)
+	}
+	return
+}
+
 // Gender Field !
 type Gender int
 
@@ -60,36 +84,21 @@ func (Gender) Female() Gender {
 	return 2
 }
 
-func (t Student) Validate() (errMsg map[string]string) {
-	// Initialize the error messages
-	errMsg = map[string]string{}
-	// Get any records from the database that macthes the name of
-	// this record and make sure the record is not the record we are
-	// editing right now
-	student := Student{}
-	if uadmin.Count(&student, "account_number <> ?", t.AccountNumber) == 0 {
-		errMsg["account_number"] = "The Student is already in the system"
-		uadmin.Trail(uadmin.DEBUG, errMsg)
-		// uadmin.ALERT("")
-	} else {
-		uadmin.Trail(uadmin.DEBUG, "The Student is not in the system.")
-	}
-	return
-}
-
 func (s *Student) Save() {
-	student := Student{}
 	uadmin.Trail(uadmin.DEBUG, s.AccountNumber)
-
-	if s.Enrolled {
-		if uadmin.Count(&student, "account_number <> ?", s.AccountNumber) != 0 {
-			s.AccountNumber = "24" + strconv.Itoa(rand.Intn(99)) + strconv.Itoa(rand.Intn(99)) + strconv.Itoa(rand.Intn(99)) + strconv.Itoa(rand.Intn(99))
-			fmt.Println(s.AccountNumber)
-		}
-		//student := Student{}
-		uadmin.Save(s)
-	} else {
-		s.AccountNumber = ""
-		uadmin.Save(s)
-	}
+	s.FullName = s.LastName + " " + s.FirstName + " "
+	uadmin.Save(s)
+	// if s.Enrolled {
+	// 	if uadmin.Count(&s, "account_number <> ?", s.AccountNumber) == 0 {
+	// 		//fmt.Println(s.AccountNumber)
+	// 		uadmin.Trail(uadmin.DEBUG, s.AccountNumber)
+	// 		uadmin.Trail(uadmin.DEBUG, "No Duplicated Acc number found. Student successfully added.")
+	// 		uadmin.Save(s)
+	// 	}
+	// 	//student := Student{}
+	// } else {
+	// 	s.AccountNumber = ""
+	// 	uadmin.Trail(uadmin.DEBUG, "Duplicate found. Adding")
+	// 	uadmin.Save(s)
+	// }
 }
